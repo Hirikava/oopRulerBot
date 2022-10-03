aefe24a85684f3da8d5de6be24a358220cfabe40
alexx <alexx@skbkontur.ru>
2022-10-03 18:55:14 +0500

    add di

---
+++ OopRulerBot/OopRulerBot/DI/BotContainerBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using OopRulerBot.Infra;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+using Vostok.Logging.File;
+using Vostok.Logging.File.Configuration;
+
+namespace OopRulerBot.DI;
+
+public static class BotContainerBuilder
+{
+    public static IContainer Build()
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.Register<ILog>(cc =>
+        {
+            var consoleLog = new ConsoleLog();
+            var fileLogSettings = new FileLogSettings
+            {
+                RollingStrategy = new RollingStrategyOptions
+                {
+                    MaxFiles = 10,
+                    MaxSize = 1024 * 1024 * 100,
+                    Period = RollingPeriod.Day,
+                    Type = RollingStrategyType.Hybrid
+                }
+            };
+            var fileLog = new FileLog(fileLogSettings);
+            return new CompositeLog(consoleLog, fileLog);
+        }).SingleInstance();
+        containerBuilder
+            .Register<IDiscordLogAdapter>(cc => new VostokDiscordLogAdapter(cc.Resolve<ILog>()))
+            .SingleInstance();
+
+        return containerBuilder.Build();
+    }
+}
--- OopRulerBot/OopRulerBot/Program.cs
+++ OopRulerBot/OopRulerBot/Program.cs
@@ -1,22 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Autofac;
 using Discord;
 using Discord.WebSocket;
+using OopRulerBot.DI;
 using OopRulerBot.Infra;
-using Vostok.Logging.Abstractions;
-using Vostok.Logging.Console;
+
+
+namespace OopRulerBot;
 
 public static class Program
 {
-
-    public const string Token = "MTAyMTM3ODQ3NjA4MDY0ODI4Mg.GPyWDe.JRoLCmeDjf2aVIsKA8Ty2o13aHt6y3Rnb1uGdM";
+    public const string Token = "MTAyMTM3ODQ3NjA4MDY0ODI4Mg.GwqFzN.bRrUmTfMQLYszKcnx4HA8hbeMd02Psxn0Mdmdw";
     public static async Task Main(string[] args)
     {
-        var consoleLog = new ConsoleLog();
-        var discordLogAdapter = new VostokDiscordLogAdapter(consoleLog);
+        var container = BotContainerBuilder.Build();
         var discordClient = new DiscordSocketClient();
-        discordClient.Log += discordLogAdapter.HandleLogEvent;
-       
+        
+        discordClient.Log += container.Resolve<IDiscordLogAdapter>().HandleLogEvent;
         await discordClient.LoginAsync(TokenType.Bot, Token);
         await discordClient.StartAsync();
         await Task.Delay(-1);
