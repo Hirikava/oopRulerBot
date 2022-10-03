a2b39d40bfd26e6038afd92c7e5b98311ba4bbd7
alexx <alexx@skbkontur.ru>
2022-10-03 20:50:31 +0500

    add message logging

--- OopRulerBot/OopRulerBot/Program.cs
+++ OopRulerBot/OopRulerBot/Program.cs
@@ -2,29 +2,41 @@
 
 using Autofac;
 using Discord;
+using Discord.Commands;
 using Discord.WebSocket;
 using OopRulerBot.DI;
 using OopRulerBot.Infra;
 using OopRulerBot.Settings;
-using Vostok.Configuration;
 using Vostok.Configuration.Abstractions;
-using Vostok.Configuration.Sources.Json;
-
+using Vostok.Logging.Abstractions;
 
 namespace OopRulerBot;
 
 public static class Program
 {
+    private static IContainer Container = null!;
+
     public static async Task Main(string[] args)
     {
-        var container = BotContainerBuilder.Build();
+        Container = BotContainerBuilder.Build();
         var discordClient = new DiscordSocketClient();
+
+        discordClient.Log += Container.Resolve<IDiscordLogAdapter>().HandleLogEvent;
+        discordClient.MessageReceived += HandleMessage;
         
-        discordClient.Log += container.Resolve<IDiscordLogAdapter>().HandleLogEvent;
-        var discordToken = container.ResolveNamed<IConfigurationProvider>(ConfigurationScopes.BotSettingsScope)
+        var discordToken = Container.ResolveNamed<IConfigurationProvider>(ConfigurationScopes.BotSettingsScope)
             .Get<BotSecretSettings>().DiscordToken;
         await discordClient.LoginAsync(TokenType.Bot, discordToken);
         await discordClient.StartAsync();
         await Task.Delay(-1);
+    }
+
+    public static Task HandleMessage(SocketMessage socketMessage)
+    {
+        var message = socketMessage as SocketUserMessage;
+        if (message == null) return Task.CompletedTask;
+        var log = Container.Resolve<ILog>();
+        log.Info("{sender} says {message}", message.Author.Username, message.Content);
+        return Task.CompletedTask;
     }
 }
