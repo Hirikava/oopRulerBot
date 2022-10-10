0f7e7ebfb206857bb0d5d9ba2ca2b28de9b9c722
Pavuuk <88192739+Pavuuk@users.noreply.github.com>
2022-10-10 17:48:52 +0500

    "Serilog"

--- OopRulerBot/OopRulerBot/Infra/SerilogDiscordLogAdapter.cs
+++ OopRulerBot/OopRulerBot/Infra/SerilogDiscordLogAdapter.cs
@@ -1,6 +1,31 @@
-﻿namespace OopRulerBot.Infra;
+﻿using Discord;
+using Serilog.Events;
 
-public class SerilogDiscordLogAdapter
+namespace OopRulerBot.Infra;
+using Serilog;
+
+public class SerilogDiscordLogAdapter : IDiscordLogAdapter
 {
-    
+    private readonly ILogger log;
+    public SerilogDiscordLogAdapter(ILogger log)
+    {
+        this.log = log.ForContext("DiscordApi", null);
+    }
+
+    public Task HandleLogEvent(LogMessage logMessage)
+    {
+        log.Write(GetLogLevel(logMessage.Severity),logMessage.Exception, logMessage.Message);
+        return Task.CompletedTask;
+    }
+
+    private LogEventLevel GetLogLevel(LogSeverity logSeverity) => logSeverity switch
+    {
+        LogSeverity.Critical => LogEventLevel.Fatal,
+        LogSeverity.Error => LogEventLevel.Error,
+        LogSeverity.Warning => LogEventLevel.Warning,
+        LogSeverity.Info => LogEventLevel.Information,
+        LogSeverity.Verbose => LogEventLevel.Information,
+        LogSeverity.Debug => LogEventLevel.Debug,
+        _ => throw new ArgumentOutOfRangeException(nameof(logSeverity), logSeverity, null)
+    };
 }
